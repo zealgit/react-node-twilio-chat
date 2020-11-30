cf78611261d9d63281f6234cbd06fd306d701d68
Neetin <neetin@zealousweb.com>
2020-11-30 13:19:44 +0530

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./App.css";
+import ChatApp from "./ChatApp.js";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>React Node.js Twilio Programmable Chat!</h1>
+        </header>
+        <ChatApp />
+      </div>
+    );
+  }
+}
+
+export default App;
